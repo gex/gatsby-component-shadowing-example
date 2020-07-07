fa135db78a5dac341726f1c5b5985782561c625c
Gergo Szonyi <github@notgex.dev>
2020-07-07 15:23:48 +0200

    customize the theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,47 @@
 const { resolve } = require('path')
 
 module.exports = {
+  siteMetadata: {
+    title: `custom nehalem`,
+    topics: [`redditors`, `bloggers`, `geeks`, `nerds`, `people`, `everyone`],
+    author: {
+      name: `gex`,
+      description: `I'm <strong>gex</strong>, an average redditor`,
+      social: {
+        facebook: '',
+        twitter: '',
+        linkedin: '',
+        instagram: '',
+        youtube: '',
+        github: 'https://github.com/gex',
+        twitch: ''
+      }
+    }
+  },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         name: 'content',
         path: resolve(__dirname, 'content')
       }
     },
     {
-      resolve: `gatsby-transformer-yaml`,
+      resolve: 'gatsby-transformer-yaml',
       options: {
-        typeName: `Tags`
+        typeName: 'Tags'
       }
     },
     {
-      resolve: `@nehalist/gatsby-theme-nehalem`,
+      resolve: '@nehalist/gatsby-theme-nehalem',
       options: {
         manifest: {
-          name: `nehalem - A Gatsby theme`,
-          short_name: `nehalem`,
-          start_url: `/`,
-          background_color: `#a4cbb8`,
-          theme_color: `#a4cbb8`,
-          display: `minimal-ui`,
+          name: 'nehalem - A Gatsby theme',
+          short_name: 'nehalem',
+          start_url: '/',
+          background_color: '#a4cbb8',
+          theme_color: '#a4cbb8',
+          display: 'minimal-ui',
           icon: resolve(__dirname, 'content/assets/nehalist-gatsby.png')
         },
         loadDefaultPages: true,
---
+++ src/@nehalist/gatsby-theme-nehalem/components/footer/index.tsx
@@ -0,0 +1,52 @@
+import React, { FunctionComponent } from 'react'
+import { MenuItem } from '../../utils/models'
+import {
+  Copyright,
+  DesignBy,
+  FooterContainer,
+  FooterMenuItem,
+  FooterMenuLink,
+  StyledFooter,
+  StyledNav
+} from './style'
+
+interface FooterProps {
+  menu: MenuItem[]
+  owner: string
+}
+
+const Footer: FunctionComponent<FooterProps> = ({ menu, owner }) => (
+  <StyledFooter>
+    <FooterContainer>
+      <StyledNav>
+        <ul>
+          {menu.map((item, index) => (
+            <li key={index}>
+              {/* Links to RSS and Sitemap are handled
+                  differently (for now) since they're technically external links */}
+              {['/rss.xml', '/sitemap.xml'].indexOf(item.path) >= 0 ? (
+                <FooterMenuItem href={item.path} rel={`noopener noreferrer`}>
+                  {item.name}
+                </FooterMenuItem>
+              ) : (
+                <FooterMenuLink to={item.path}>{item.name}</FooterMenuLink>
+              )}
+            </li>
+          ))}
+        </ul>
+      </StyledNav>
+      <div>
+        <Copyright>
+          <strong>gex 😎</strong>&nbsp;&copy; {new Date().getFullYear()}
+        </Copyright>
+        <DesignBy>
+          <a href={`https://notgex.com`} target={`_blank`} rel={`noopener`}>
+            notgex.com
+          </a>
+        </DesignBy>
+      </div>
+    </FooterContainer>
+  </StyledFooter>
+)
+
+export default Footer
